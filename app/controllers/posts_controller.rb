da7d1249624c56a87462c2364412e7f31d51470b
Szhangjinghua <stevezjhg@gmail.com>
2017-08-28 09:57:11 +0800

    implement my posts_CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,29 @@
 class PostsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :find_group
 
   def new
-    @group = Group.find(params[:group_id])
     @post = Post.new
   end
+def edit
+  @post = @group.posts.find(params[:id])
+end
+def update
+  @post = @group.posts.find(params[:id])
+  if @post.update(post_params)
+    redirect_to group_path(@group)
+  else
+    render :edit
+  end
+end
+def destroy
+  @post = @group.posts.find(params[:id])
 
+  @post.destroy
+  redirect_to group_path(@group)
+end
   def create
-    @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
@@ -26,5 +41,7 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:content)
   end
-
+def find_group
+  @group = Group.find(params[:group_id])
+end
 end
